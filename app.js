6cc2d849abc7a3ac26437d0c6b6cd80df59b5a9f
alikahwaji <ali.kahwaji@hotmail.com>
2017-08-22 01:12:35 +1200

    Rendering Input data

--- app.js
+++ app.js
@@ -5,18 +5,38 @@ var budgetController = (function () {
 
 var UIController = (function () {
 
-// Code 
-
+  var DOMStrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  }
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMStrings.inputType).value,
+        description: document.querySelector(DOMStrings.inputDescription).value,
+        value: document.querySelector(DOMStrings.inputValue).value
+      }
+    },
+    getDOMStrings: function () {
+      return DOMStrings
+    }
+  }
 })()
 
 var controller = (function (budgetCtrl, UICtrl) {
 
+  var DOM = UICtrl.getDOMStrings()
+
   var ctrlAddItem = function () {
-    console.log('Pressed')
+    var input = UICtrl.getInput()
+    console.log(input)
+
 
   }
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
 
   document.addEventListener('keypress', function (e) {
     if (e.keyCode === 13 || e.which === 13) {
