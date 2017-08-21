2ec4471633c01ed8f894d9b84a2f0b47f567f16f
alikahwaji <ali.kahwaji@hotmail.com>
2017-08-22 00:23:14 +1200

    Creating eventListener

--- app.js
+++ app.js
@@ -10,5 +10,19 @@ var UIController = (function () {
 })()
 
 var controller = (function (budgetCtrl, UICtrl) {
-// Code
+
+  var ctrlAddItem = function () {
+    console.log('Pressed')
+
+  }
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+
+  document.addEventListener('keypress', function (e) {
+    if (e.keyCode === 13 || e.which === 13) {
+      ctrlAddItem()
+    }
+
+  })
+  
 })(budgetController, UIController)
